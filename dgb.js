c07241883c271d8d86cf37b8e8bdea77be5bb938
mj <inchingforward@gmail.com>
2015-01-19 13:15:36 -0600

    Added level drawing.

--- dgb.js
+++ dgb.js
@@ -1,9 +1,24 @@
 window.onload = function() {
+    var numRows = numCols = 12;
     var imageDim = 30;
-    var worldDim = imageDim * 12;
+    var worldDim = imageDim * numCols;
     var game = new Phaser.Game(worldDim, worldDim, Phaser.AUTO, '', { preload: preload, create: create, update: update });
     var allowInput = false;
     var player, vampire, exit;
+    
+    var level = 
+        '...........' + 
+        '............' + 
+        '............' + 
+        '............' + 
+        '............' + 
+        '.P...+....V.' + 
+        '............' + 
+        '............' + 
+        '............' + 
+        '.....X......' + 
+        '............' + 
+        '............';
 
     function preload () {
         game.load.image('player', 'assets/player.png');
@@ -12,19 +27,45 @@ window.onload = function() {
         game.load.image('exit', 'assets/exit_tile.png');
         game.load.image('wall', 'assets/wall_tile.png');
     }
-
+    
     function create () {
-        game.stage.backgroundColor = '#ccc';
+        game.stage.backgroundColor = '#dddddd';
         
-        // Adjust center to image offset.
-        var centerX = game.world.centerX - (imageDim / 2);
-        var centerY = game.world.centerY - (imageDim / 2);
-        
-        player = game.add.sprite(centerX, centerY, 'player');
-        vampire = game.add.sprite(centerX + (imageDim * 2), centerY, 'vampire');        
-        exit = game.add.sprite(centerX, 0, 'exit');
+        createLevel(level);
         
         allowInput = true;
+    }
+    
+    function createLevel(level) {
+        for (var i=0; i < level.length; ++i) {
+            var row = i % numRows;
+            var col = Math.floor(i / numCols);
+            
+            var x = imageDim * row;
+            var y = imageDim * col;
+            
+            decodeAndPlaceSprite(level.charAt(i), x, y);
+        }
+        
+        player.bringToTop();
+        vampire.bringToTop();
+    }
+    
+    function decodeAndPlaceSprite(str, x, y) {
+        if (str == 'P') {  // Player
+            game.add.sprite(x, y, 'empty');
+            player = game.add.sprite(x, y, 'player');
+        } else if (str == 'V') {  // Vampire
+            game.add.sprite(x, y, 'empty');
+            vampire = game.add.sprite(x, y, 'vampire');
+        } else if (str == 'X') {  // Exit
+            game.add.sprite(x, y, 'empty');
+            exit = game.add.sprite(x, y, 'exit');
+        } else if (str == '.') {  // Empty
+            game.add.sprite(x, y, 'empty');
+        } else if (str == '+') {  // Wall
+            game.add.sprite(x, y, 'wall');
+        }
     }
     
     function update() {
@@ -46,6 +87,10 @@ window.onload = function() {
     function movePlayerAndUpdateVampire(x, y) {
         movePlayer(x, y);
         updateVampire();
+    }
+    
+    function canMovePlayerTo(x, y) {
+        return true;
     }
     
     function movePlayer(x, y) {
